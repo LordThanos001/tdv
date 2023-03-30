c0ebd7d69ebe70a0a5f4a9d3177840d6092cd3c1
LordThanos001 <94503577+LordThanos001@users.noreply.github.com>
2023-03-30 23:55:11 +0100

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['tdv-production.up.railway.app']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
